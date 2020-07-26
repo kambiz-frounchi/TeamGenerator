fa052d84c007cee2c4769c960d969dd46eb40d01
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-07-26 18:55:15 -0400

    classes defined, tests pass

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,34 @@
-// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor (name, id, email) {
+        this.role = `Employee`;
+        if (name) {
+            this.name = name;
+        }
+
+        if (id) {
+            this.id = id;
+        }
+        
+        if (email) {
+            this.email = email;
+        }
+    }
+
+    getName () {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return this.role;
+    }
+}
+
+module.exports = Employee;
